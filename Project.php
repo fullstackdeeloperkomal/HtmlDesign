45104c1d615c6ba411bdf0ead5ce13e14f9cbe83
fullstackdeeloperkomal <73990815+fullstackdeeloperkomal@users.noreply.github.com>
2020-12-15 22:07:22 +0530

    Add files via upload

---
+++ Project.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+       .pink{
+           height: 650px;
+           width: 445px;
+           background-color: pink;
+           float: left;
+       }
+       .yellow{
+           height: 650px;
+           width: 445px;
+           background-color:yellow;
+           float: left;
+       }
+       .orange{
+           height: 650px;
+           width: 445px;
+           background-color:orange;
+           float: left;
+       }
+
+
+    </style>
+</head>
+<body>
+    <a href="Formm.php"><div class="pink"></div></a>
+    <a href="Formm.php"><div class="yellow"></div></a>
+    <a href="#"><div class="orange"></div></a>
+</body>
+</html>
